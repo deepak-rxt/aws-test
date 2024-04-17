821992e65b997b92913c6939e33d8fb9cc5a9418
deepak-rxt <98146577+deepak-rxt@users.noreply.github.com>
2024-04-17 16:57:39 +0530

    Update credes.py

--- credes.py
+++ credes.py
@@ -1,4 +1,6 @@
-aws s3api list-buckets --query "Buckets[?Name=='athena-access-test-bucket-april-2'].Name" --output text
+s3=$(aws s3api list-buckets --query "Buckets[?Name=='athena-access-test-bucket-april-2'].Name" --output json)
+s3_count=$(echo "$s3" | jq 'if type == "array" then length else 0 end')
+echo "$s3_count"
 
 ec2_client = boto3.client('ec2',
                               aws_access_key_id=credentials['AccessKeyId'],
