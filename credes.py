fe734b70928ed7913808598e05bb105f331f7a5b
deepak-rxt <98146577+deepak-rxt@users.noreply.github.com>
2024-04-17 16:29:16 +0530

    Update credes.py

--- credes.py
+++ credes.py
@@ -1,4 +1,6 @@
-  ec2_client = boto3.client('ec2',
+aws s3api list-buckets --query "Buckets[?starts_with(Tags[?Key=='Name'] | [0].Value, '$tag_value')].Name" --output text
+
+ec2_client = boto3.client('ec2',
                               aws_access_key_id=credentials['AccessKeyId'],
                               aws_secret_access_key=credentials['SecretAccessKey'],
                               aws_session_token=credentials['SessionToken'],
